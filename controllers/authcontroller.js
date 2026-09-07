bd4e4ffa152e75fd6e148cf8c5b0734df2f5448a
smitimishra <Smriti.mishra@peopleprocessteck.com>
2026-09-07 12:56:03 +0530

    issue fixed

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -591,7 +591,7 @@ const forgotPassword = async (req, res) => {
         // ----------------------------------------------------
 
         const resetUrl =
-            `http://192.168.29.51:4200/reset-password?token=${resetToken}`;
+            `http://192.168.29.216:4200/reset-password?token=${resetToken}`;
 
 
         // ----------------------------------------------------
